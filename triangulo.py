bca6fb51fc1a16217b9822930e7e4ef1c114368f
Diogenes Melgaco <diogenes.ti@outlook.com>
2020-05-24 16:24:39 -0300

    Adicionando triangulo.py

---
+++ triangulo.py
@@ -0,0 +1,34 @@
+ladoa = int(input('Digite o valor do Lado A:\n'))
+while ladoa < 1:
+    print('Digite um valor maior que 0.')
+    ladoa = int(input('Digite o valor do Lado A:\n'))
+
+ladob = int(input('Digite o valor do Lado B:\n'))
+while ladob < 1:
+    print('Digite um valor maior que 0.')
+    ladob = int(input('Digite o valor do Lado B:\n'))
+
+ladoc = int(input('Digite o Valor do Lado C:\n'))
+while ladoc < 1:
+    print('Digite um valor maior que 0.')
+    ladoc = int(input('Digite o Valor do Lado C:\n'))
+
+v1 = (ladoa + ladob)
+v2 = (ladoa + ladoc)
+v3 = (ladob + ladoc)
+
+if ladoa < v3:
+    resultado = True
+elif ladob < v2:
+    resultado = True
+elif ladoc < v1:
+    resultado = True
+else:
+    resultado = False
+
+if resultado == True:
+    print('É um triangulo')
+else:
+    print('Não é um triangulo')
+
+'teste'
